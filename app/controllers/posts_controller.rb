78ee3936b1af87a9a8e957b6ce56079cd195d7a5
ryan <ryan.haberle@outlook.com>
2019-04-20 02:10:08 -0400

    new commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,27 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post = Post.new
     end
 
-    def create
+    def create  #create a new post
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else 
+            render 'new'
+        end
     end
+
+    def show
+        @post = Post.find(params[:id])
+end
+
+private
+
+def post_params   # vaidation for parameters passed through post method
+    params.require(:post).permit(:title, :content)
+end
+
 end
